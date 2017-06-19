9054ed8e8a7f614d78da18ac6b7a68e7414586f8
Rúben Freitas <emanuelfreitas477@gmail.com>
2017-06-19 11:19:30 +0100

    Add header styles

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from './header';
+
+const AppWrapper = styled.div`
+  font-family: 'Roboto', sans-serif;
+  padding: 0 15px;
+  max-width: 1024px;
+  margin: 0 auto;
+  width: 100%;
+`;
 
 export default function () {
   return (
-    <div>
+    <AppWrapper>
       <Header />
-    </div>
+    </AppWrapper>
   );
 }
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
 import SponsorBlock from '../containers/sponsor_block';
+
+const Header = styled.header`
+  padding-bottom: 20px;
+  border-bottom: 1px solid grey;
+`;
+
+const Title = styled.h1`
+  font-size: 44px;
+  font-weight: normal;
+  text-align: center;
+
+  @media (max-width: 600px) {
+    font-size: 24px;
+  }
+`;
+
+const DefaultParagraph = styled.p`
+  font-size: 16px;
+  font-weight: lighter;
+`;
 
 export default function () {
   return (
-    <header>
-      <h1>Discount Ascii Warehouse</h1>
-      <p>
+    <Header>
+      <Title>Discount Ascii Warehouse</Title>
+      <DefaultParagraph>
         Here you&apos;re sure to find a bargain on some of the finest ascii
         available to purchase.
         Be sure to peruse our selection of ascii faces in an exciting range of
         sizes and prices.
-      </p>
+      </DefaultParagraph>
+      <DefaultParagraph>
+        But first, a word from our sponsors:
+      </DefaultParagraph>
       <SponsorBlock />
-    </header>
+    </Header>
   );
 }
