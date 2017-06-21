8a40e9212ef25bcaa1644eeae7ae1ad4237e8a12
Rúben Freitas <emanuelfreitas477@gmail.com>
2017-06-21 19:52:30 +0100

    Update missing test cases on catalog full reducer

--- frontend/test/reducers/catalog_full_test.js
+++ frontend/test/reducers/catalog_full_test.js
@@ -11,10 +11,14 @@ export default () => {
     it('handles action of type NEW_SEARCH / ADD TO SEARCH with response data', () => {
       const action = { type: NEW_SEARCH, payload: { data: '{"id":"1"}\n{"id":"2"}\n' } };
       expect(catalogFull(false, action)).to.eql(false);
+      const action2 = { type: ADD_TO_SEARCH, payload: { data: '{"id":"1"}\n{"id":"2"}\n' } };
+      expect(catalogFull(false, action)).to.eql(false);
     });
 
     it('handles action of type NEW_SEARCH / ADD TO SEARCH without response data', () => {
       const action = { type: ADD_TO_SEARCH, payload: { data: '' } };
+      expect(catalogFull(false, action)).to.eql(true);
+      const action2 = { type: NEW_SEARCH, payload: { data: '' } };
       expect(catalogFull(false, action)).to.eql(true);
     });
   });
